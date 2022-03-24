c2da946bd9593a5865a64774245d478f05115d33
Em Grasmeder <emma@gras.org>
2022-03-24 23:33:13 +0100

    Add multimethod update-event-log in order to differentiate between the two event types

--- src/air_traffic_control/core.clj
+++ src/air_traffic_control/core.clj
@@ -1,23 +1,20 @@
 (ns air-traffic-control.core
-  (:require [air-traffic-control.events :as events]
-            [clojure.spec.alpha :as s]
+  (:require [clojure.spec.alpha :as s]
             [clojure.string :as clj-str]))
 
 (defn parse-event [s]
   (let [maybe-event (clj-str/split s #" ")]
-    (println (s/conform :events/timestamp (second maybe-event)))
-    (s/conform :events/event maybe-event)))
+    (s/conform :air-traffic-control.events/event maybe-event)))
+
+(defmulti update-event-log first)
+
+(defmethod update-event-log :update [[_ {:keys [plane-id model origin destination event-type timestamp fuel-delta]}]]
+  (println "i'm parsing a thing as an update with"   plane-id model origin destination event-type timestamp fuel-delta))
+
+(defmethod update-event-log :removal [[_ {:keys [plane-id timestamp]}]]
+  (println "i'm parsing a thing as a removal with"   plane-id timestamp))
+
+(update-event-log (parse-event "F222 2021-03-29T10:00:00"))
+(update-event-log (parse-event "F324 313 LONDON NEWYORK Take-Off 2021-03-29T12:00:00 0"))
     
-
 (s/explain :events/removal (clj-str/split "F222 2020-03-03T10:00:00" #" "))
-
-
-
-
-#_(defn parse-airplane-status 
-    "Turns a single airplane status event into a map for easier use. 
-  Probably a good use case for defrecord"
-    [airplane-status]
-    (zipmap [:plane-id :plane-model :origin :destination :event-type :timestamp :fuel-delta]
-     (s/split airplane-status #" ")))
-
